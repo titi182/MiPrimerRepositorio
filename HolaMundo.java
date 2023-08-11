17341321679a734a7ce70074f34b16be6e3b4022
titi182 <130761746+titi182@users.noreply.github.com>
2023-08-11 10:02:26 -0500

    Add files via upload
    este es mi primer repositorio

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author cristina
+ */
+// clase principal de java 
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Cristina");
+
+    }
+
+}
